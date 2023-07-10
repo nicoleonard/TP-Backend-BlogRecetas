fb02888bd87b05113f16f1915c13ff8307e98fa5
nicoleonard <nikolasleonard@gmail.com>
2023-07-10 15:55:47 -0300

    añadida instancia de express y configuracion para toma de puerto. agregado middleware para conexiones remotas y para usar datos del request en formato json

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 import express from 'express';
-console.log("Estructura inicial backend.");
+import cors from 'cors';
+
+const instanciaExpress = express();
+
+
+instanciaExpress.set('puerto', process.env.PORT || 4000) // notas 1: configura una variable 'puerto' que toma el valor de una variable de entorno o usa el puerto 4000
+instanciaExpress.listen(instanciaExpress.get('puerto'), ()=>{console.log('Estoy en el puerto '+instanciaExpress.get('puerto'))}) // toma el puerto que coincida con el valor de la variable 'puerto'
+
+instanciaExpress.use(cors()); // permite conexiones remotas
+instanciaExpress.use(express.json()) // permite extraer datos del request en formato json
