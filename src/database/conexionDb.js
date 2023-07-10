8ec6fcb3f52c83bb6413ac5582da3d729992d12a
nicoleonard <nikolasleonard@gmail.com>
2023-07-10 18:47:24 -0300

    establecida conexcion con la base de datos

---
+++ src/database/conexionDb.js
@@ -0,0 +1,6 @@
+import { connect } from 'mongoose';
+import { MONGODB_URI } from '../config';
+
+connect(MONGODB_URI, {family: 4})
+.then((resp) => console.log(`Base de datos conectada en ${resp.connection.name}`))
+.catch((error) => console.log(error))
