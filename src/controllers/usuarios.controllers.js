25819a51233558db0a92576721d345f8775c81df
nicoleonard <nikolasleonard@gmail.com>
2023-07-10 21:40:34 -0300

    añadida funcion para obtener usuarios de la base de datos

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,3 +1,11 @@
-export const obtenerUsuarios = (req,res)=>{
-    res.send("peticion realizada a usuarios")
+import Usuario from "../models/usuarios"
+
+export const obtenerUsuarios = async (req,res)=>{
+    try{
+        const usuarios = await Usuario.find() // mi modelo se encarga de encontrar en la base de datos todos los usuarios
+        res.status(200).json(usuarios)
+    } catch (error){
+        console.log(error)
+        res.status(404).json({mensaje: 'No se puedieron buscar usuarios'})
+    }
 }
