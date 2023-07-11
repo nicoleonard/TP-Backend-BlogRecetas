e585f1e07f026ea9d19b370e9915e1ac85de8b6e
nicoleonard <nikolasleonard@gmail.com>
2023-07-10 22:07:23 -0300

    añadida funcion para obtener recetas de la base de datos

--- src/controllers/recetas.controllers.js
+++ src/controllers/recetas.controllers.js
@@ -1,3 +1,11 @@
-export const obtenerRecetas = (req,res)=>{
-    res.send("peticion realizada a recetas")
+import Receta from "../models/recetas"
+
+export const obtenerRecetas = async (req,res)=>{
+    try{
+        const recetas = await Receta.find() // mi modelo se encarga de encontrar en la base de datos todas las recetas
+        res.status(200).json(recetas)
+    } catch (error){
+        console.log(error)
+        res.status(404).json({mensaje: 'No se puedieron buscar recetas'})
+    }
 }
