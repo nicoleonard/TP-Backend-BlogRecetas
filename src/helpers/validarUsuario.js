5f7cf3c21d7b48a1349e95f7438947824d238573
nicoleonard <nikolasleonard@gmail.com>
2023-07-11 17:50:00 -0300

    añadidas funciones para validar usuarios. creado helper para validar usuarios

---
+++ src/helpers/validarUsuario.js
@@ -0,0 +1,32 @@
+import { check } from "express-validator";
+import resultadoValidacion from "./resultadoValidacion";
+
+const validarUsuario = [
+    check("usuario")
+        .notEmpty()
+        .withMessage("El nombre de usuario es requerido")
+        .isLength({ min: 5, max: 100 })
+        .withMessage(
+            "El nombre de usuario debe tener entre 5 y 100 caracteres"
+        ),
+    check("clave")
+        .notEmpty()
+        .withMessage("La clave es requerida")
+        .isLength({ min: 4, max: 100 })
+        .withMessage(
+            "La clave debe tener entre 4 y 100 caracteres")
+        .matches(
+            /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,64})/)
+        .withMessage('La contraseña puede tener de 8 a 64 caracteres y contiene una mezcla de mayusculas y minusculas, un numero y un caracter especial'
+        ),
+
+    check("tipo")
+        .notEmpty()
+        .withMessage('Error en el tipo de usuario')
+        .isIn(["admin", "usuario"])
+        .withMessage('Error en el tipo de usuario'
+        ),
+    (req, res, next) => { resultadoValidacion(req, res, next) }
+];
+
+export default validarUsuario;
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { crearUsuario, obtenerUsuarios } from '../controllers/usuarios.controllers';
+import validarUsuario from '../helpers/validarUsuario';
 
 const router = Router();
 
-router.route('/usuarios').get(obtenerUsuarios).post(crearUsuario); // a la ruta /usuarios le puedo hacer las peticiones que aqui se asocian con la funcion a la que invocan (se comunica rutas con controladores)
+router.route('/usuarios').get(obtenerUsuarios).post(validarUsuario, crearUsuario); // a la ruta /usuarios le puedo hacer las peticiones que aqui se asocian con la funcion a la que invocan (se comunica rutas con controladores)
 export default router;
